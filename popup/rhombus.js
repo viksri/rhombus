fc3e84c0d080b9cbc8824692379e56fe17636e20
Ayush Gupta <ayush@sumologic.com>
2018-01-19 23:12:37 +0530

    Rhombus-1: Add + and x buttons

--- popup/rhombus.js
+++ popup/rhombus.js
@@ -1,5 +1,11 @@
+var currentInputId = '1'
 var x = initialize();
 var outer = document.getElementById("outer");
+
+var addNewTimeButton = document.getElementById("addContainer");
+addNewTimeButton.addEventListener("click", function() {
+  createNextInput(currentInputId + 1);
+});
 
 function initialize() {
   var currentEpochBox = document.getElementById("currentEpoch");
@@ -10,14 +16,15 @@ function initialize() {
   var currentEpochValue = currentTimeValue.getTime();
   currentEpochBox.textContent = "Current epoch: " + currentEpochValue;
 
-  var start = getInput('1');
+  var start = getInput(currentInputId);
 }
 
 function getInput(id) {
   var box = document.getElementById("input" + id);
-  box.addEventListener("input", function() { 
-    addOutput(box, id); 
+  box.addEventListener("input", function() {
+    addOutput(box, id);
   });
+  currentInputId += 1;
 }
 
 function addOutput(box, id) {
@@ -26,42 +33,13 @@ function addOutput(box, id) {
 
   var epochBox = createOutputElement(container, "epoch" + id);
   var timeBox = createOutputElement(container, "time" + id);
-  
+
   timeBox.textContent = "Time: " + result[0];
   epochBox.textContent = "Epoch: " + result[1];
-
-  var nextId = id + 1;
-  var next = createNextInput(nextId);
 }
 
-function getOutput(inputBox) {
-  var inputValue = inputBox.value;
-  if (inputValue.length == 0) {
-    return ["", ""];
-  } else if(isEpoch(inputValue)) {
-    var timeValue = new Date(0);
-    timeValue.setUTCSeconds(inputValue/1000);
-    return [timeValue, inputValue];
-  } else {
-    return ["Invalid date", "Invalid date"];
-  }
-}
-
-function createOutputElement(container, id) {
-  var outputBox = document.getElementById(id);
-  
-  if (outputBox == null) {
-    outputBox = document.createElement("output");
-    outputBox.setAttribute("id", id);
-    outputBox.style.fontFamily = "Calibri"; 
-    outputBox.style.fontSize = "14px"; 
-    outputBox.style.margin = "2px 2px 2px 2px";
-
-    container.appendChild(document.createElement("br"))
-    container.appendChild(outputBox);
-  }
-
-  return outputBox;
+function isEpoch(value) {
+  return /^\d+$/.test(value);
 }
 
 function createNextInput(id) {
@@ -73,18 +51,61 @@ function createNextInput(id) {
 
     var nextInput = document.createElement("input");
     nextInput.setAttribute("id", "input" + id);
-    nextInput.style.fontFamily = "Calibri"; 
-    nextInput.style.fontSize = "14px"; 
+    nextInput.style.fontFamily = "Calibri";
+    nextInput.style.fontSize = "14px";
     nextInput.style.margin = "2px 2px 2px 2px";
-    nextInput.style.border = "1px solid #999999"; 
+    nextInput.style.border = "1px solid #999999";
+
+    var nextCancel = document.createElement("button");
+    nextCancel.setAttribute("id", "cancel" + id);
+    nextCancel.setAttribute("class", "buttonx");
+    nextCancel.innerHTML = "x";
+    nextCancel.addEventListener("click", function () {
+      removeElement(id);
+    })
 
     outer.appendChild(nextContainer);
     nextContainer.appendChild(nextInput);
-    
+    nextContainer.appendChild(nextCancel);
+
     getInput(id);
   }
 }
 
-function isEpoch(value) {
-  return /^\d+$/.test(value);
+function removeElement(id) {
+  var container = document.getElementById("container" + id);
+  container.remove();
 }
+
+function createOutputElement(container, id) {
+  var outputBox = document.getElementById(id);
+
+  if (outputBox == null) {
+    outputBox = document.createElement("output");
+    outputBox.setAttribute("id", id);
+    outputBox.style.fontFamily = "Calibri";
+    outputBox.style.fontSize = "14px";
+    outputBox.style.margin = "2px 2px 2px 2px";
+
+    container.appendChild(document.createElement("br"))
+    container.appendChild(outputBox);
+  }
+
+  return outputBox;
+}
+
+
+function getOutput(inputBox) {
+  var inputValue = inputBox.value;
+  if (inputValue.length == 0) {
+    return ["", ""];
+  } else if (isEpoch(inputValue)) {
+    var timeValue = new Date(0);
+
+    timeValue.setUTCSeconds(inputValue / 1000);
+    return [timeValue, inputValue];
+  } else {
+    return ["Invalid date", "Invalid date"];
+  }
+}
+
